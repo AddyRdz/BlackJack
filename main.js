f23ca6b8508a81050dee0bcd9a6bb7dcfe9a3418
Addy Rodriguez <rdz.33.a@gmail.com>
2022-01-04 16:56:01 -0600

    updated some code

--- main.js
+++ main.js
@@ -1,4 +1,41 @@
-console.log('js loaded')
+console.log('Cards are loaded')
+// Independent Array
+const suite = ["Spade", "Club", "Diamond", "Hearts"];
+// console.log(suite)
+const cards = [1,2,3,4,5,6,7,8,9,10];
+// console.log(cards)
 
-let numbers = [1,2,3,4,5,6,7,8,9,10];
-console.log(numbers)
+const deck = []
+const playerHand = []
+const dealerHand = []
+
+// Calling the function
+for(let i=0; i<suite.length;i++){
+    // console.log(suite[i])
+}
+
+
+for (let i=0; i<cards.length;i++){
+    // console.log(cards[i])
+}
+
+// Create Deck
+for (let i=0; i<suite.length;i++){
+    for(let j=0; j<cards.length;j++){
+        // console.log(suite[i]+cards[j])
+        deck.push(suite[i]+cards[j])
+    }
+}
+
+// Want to have function that removes random card from deck and returns it. (if, else maybe)
+//  need variables 
+
+function dealCards (){
+
+}
+
+// for each button need the queryselector and a eventhandler function
+
+const buttonHitMe =document.querySelector('.buttonHitMe')
+console.log(buttonHitMe)
+
